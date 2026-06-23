3b2ea44a4a369e52d4deb7598d8c94d1b6c830d4
ginaferg <139098448+gfergusonQA@users.noreply.github.com>
2026-06-23 11:00:31 -0700

    updated module tt name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_test_gina_clock (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,6 +15,16 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+  reg [23:0] counter;
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+      counter <= 24'b0;
+    else
+      counter <= counter + 1;
+  end
+
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
